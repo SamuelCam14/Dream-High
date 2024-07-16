f5563413e6b350ff805b05a98cf9905e093300e0
Jose Samuel Camargo Sanchez <135378457+SamuelCam14@users.noreply.github.com>
2024-07-16 17:34:56 -0600

    Update script.js

--- script.js
+++ script.js
@@ -88,3 +88,17 @@ window.addEventListener('scroll', () => {
 document.addEventListener("DOMContentLoaded", function () {
   cargarProductos();
 });
+
+// HERO CONTAINER
+document.addEventListener("DOMContentLoaded", function () {
+  const letters = document.querySelectorAll('.heading .letter');
+
+  letters.forEach((letter, index) => {
+    setTimeout(() => {
+      letter.style.transition = `opacity 0.5s ease, transform 0.5s ease, filter 0.5s ease`;
+      letter.style.opacity = 1;
+      letter.style.transform = 'translateX(0) translateY(0)';
+      letter.style.filter = 'blur(0)';
+    }, index * 70);
+  });
+});
