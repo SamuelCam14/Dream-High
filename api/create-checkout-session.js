7513bf7da607483dbb237711166ec82b478901b4
Samuel Camargo <progreso.dev@proton.me>
2024-09-16 10:44:06 -0600

    meses v1.05 shipping

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -3,7 +3,45 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 module.exports = async (req, res) => {
     if (req.method === 'POST') {
         try {
-            const { name, price } = req.body;
+            const { name, price, address } = req.body;
+
+            // Define las opciones de envío
+            const shippingOptions = [
+                {
+                    shipping_rate_data: {
+                        type: 'fixed_amount',
+                        fixed_amount: {
+                            amount: 15000, // Costo de envío en centavos (por ejemplo, 150 MXN)
+                            currency: 'mxn',
+                        },
+                        display_name: 'Envío Estándar',
+                        delivery_estimate: {
+                            minimum: {
+                                unit: 'business_day',
+                                value: 5,
+                            },
+                            maximum: {
+                                unit: 'business_day',
+                                value: 7,
+                            },
+                        },
+                    },
+                },
+            ];
+
+            // Agrega la opción de envío local si la dirección es en Chihuahua, Chihuahua
+            if (address && address.city === 'Chihuahua' && address.state === 'Chihuahua') {
+                shippingOptions.push({
+                    shipping_rate_data: {
+                        type: 'fixed_amount',
+                        fixed_amount: {
+                            amount: 5000, // Costo de envío en centavos (por ejemplo, 50 MXN)
+                            currency: 'mxn',
+                        },
+                        display_name: 'Entrega local Chihuahua',
+                    },
+                });
+            }
 
             const session = await stripe.checkout.sessions.create({
                 payment_method_types: ['card', 'oxxo'],
@@ -27,6 +65,10 @@ module.exports = async (req, res) => {
                         }
                     }
                 },
+                shipping_address_collection: {
+                    allowed_countries: ['MX'], // Lista de países permitidos para la dirección de envío
+                },
+                shipping_options: shippingOptions,
                 success_url: `${req.headers.origin}/success.html?session_id={CHECKOUT_SESSION_ID}`,
                 cancel_url: `${req.headers.origin}/cancel.html`,
             });
